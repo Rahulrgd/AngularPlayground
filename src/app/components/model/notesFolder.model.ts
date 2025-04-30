6ef9f39a62439543d6e3a494648b38820b7e6330
Rahul Gupta <Rahul@avics.co.in>
2025-04-30 19:36:33 +0530

    fix: ui changes in notes feature

--- src/app/components/model/notesFolder.model.ts
+++ src/app/components/model/notesFolder.model.ts
@@ -1,3 +1,4 @@
+import { Note } from "./note.model";
 import { NoteList } from "./noteList.model";
 
 export class NotesFolder {
@@ -5,16 +6,23 @@ export class NotesFolder {
   public updatedAt: Date;
   public id?: number;
   public notesList: NoteList[];
+  public notes: Note[];
   public expanded: boolean = false; // 👈 add this
 
   constructor(public folderName: string) {
     this.createdAt = new Date();
     this.updatedAt = new Date();
     this.notesList = []; // one-to-many relation
+    this.notes = [];
   }
 
   addNoteList(noteList: NoteList) {
     this.notesList.push(noteList);
+    this.updatedAt = new Date();
+  }
+  
+  addNote(note: Note) {
+    this.notes.push(note);
     this.updatedAt = new Date();
   }
 }
